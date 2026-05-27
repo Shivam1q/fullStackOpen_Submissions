f2f0060ab470a7ebcfb6f55eb33626fbb5747b48
Shivam Yadav <shivam_y@bt.iitr.ac.in>
2026-05-27 18:05:41 +0530

    part_1b(1.3) exercise have finished.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,21 +3,34 @@ import Content from "./components/Content";
 import Total from "./components/Total";
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercise1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercise2 = 7;
-  const part3 = 'State of a component';
-  const exercise3 = 14
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
-  return(
+  return (
     <div>
-      <Header content = {course} />
-      <Content part1 = {part1} exercise1 = {exercise1} part2 = {part2} exercise2 = {exercise2} part3 = {part3} exercise3 = {exercise3} />
-      <Total total = {exercise1 + exercise2 + exercise3} />
+      <Header content={course} />
+      <Content
+        part1={part1.name}
+        exercise1={part1.exercises}
+        part2={part2.name}
+        exercise2={part2.exercises}
+        part3={part3.name}
+        exercise3={part3.exercises}
+      />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
-  )
-}
+  );
+};
 
-export default App; 
+export default App;
