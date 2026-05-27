bd5a36af91002900547a870ba5489d5817c96f87
Shivam Yadav <shivam_y@bt.iitr.ac.in>
2026-05-27 18:11:50 +0530

    part_1b(1.5) exercise have finished.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,34 +3,38 @@ import Content from "./components/Content";
 import Total from "./components/Total";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header content={course} />
+      <Header content={course.name} />
       <Content
-        part1={parts[0].name}
-        exercise1={parts[0].exercises}
-        part2={parts[1].name}
-        exercise2={parts[1].exercises}
-        part3={parts[2].name}
-        exercise3={parts[2].exercises}
+        part1={course.parts[0].name}
+        exercise1={course.parts[0].exercises}
+        part2={course.parts[1].name}
+        exercise2={course.parts[1].exercises}
+        part3={course.parts[2].name}
+        exercise3={course.parts[2].exercises}
       />
-      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Total
+        total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
+      />
     </div>
   );
 };
