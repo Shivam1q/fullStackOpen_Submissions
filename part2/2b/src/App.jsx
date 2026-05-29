4bbf46cabb4ebb91e6198b101c1e12d1f2a75854
Shivam Yadav <shivam_y@bt.iitr.ac.in>
2026-05-29 14:04:46 +0530

    part_2b(2.9) exercise is finished.

--- part2/2b/src/App.jsx
+++ part2/2b/src/App.jsx
@@ -1,41 +1,63 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number : 1785125152
-    }
-  ]); 
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState(0);
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+
   const handleInputChange = (event) => {
     setNewName(event.target.value);
-  }
+  };
 
   const handleInputNumberChange = (event) => {
     setNewNumber(event.target.value);
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(persons.find(obj => obj.name === newName)){
+    if (persons.find((obj) => obj.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       return;
+    }
+    const newPersonObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
     };
-    const newPersonObject = {"name" : newName, "number" : newNumber};
     const newPersonArray = persons.concat(newPersonObject);
     setPersons(newPersonArray);
-  }
+  };
+
+  const handleSearch = (event) => {
+    setSearchInput(event.target.value);
+    const result = persons.filter(
+      (person) =>
+        person.name.toLowerCase().includes(event.target.value.toLowerCase()) ||
+        person.number.includes(event.target.value),
+    );
+
+    setSearchArray(result);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searchInput} onChange={handleSearch} />
+      </div>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input type="text" value={newName} onChange={handleInputChange} />
+          name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
-          number : <input type="number" value={newNumber} onChange={handleInputNumberChange} />
+          number :{" "}
+          <input value={newNumber} onChange={handleInputNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -43,14 +65,32 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person, i) => {
-          return (
-            <p key={i}>{person.name} {person.number}</p>
-          )
-        })}
+        {searchInput === ""
+          ? persons.map((person) => {
+              return (
+                <p key={person.id}>
+                  {person.name} {person.number}
+                </p>
+              );
+            })
+          : persons
+              .filter(
+                (person) =>
+                  person.name
+                    .toLowerCase()
+                    .includes(searchInput.toLowerCase()) ||
+                  person.number.includes(searchInput),
+              )
+              .map((person) => {
+                return (
+                  <p key={person.id}>
+                    {person.name} {person.number}
+                  </p>
+                );
+              })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
