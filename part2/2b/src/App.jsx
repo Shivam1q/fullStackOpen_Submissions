4e43d58d31bc9d6e7abd00869fe4072eb897bc51
Shivam Yadav <shivam_y@bt.iitr.ac.in>
2026-05-29 17:20:30 +0530

    part_2c(2.11) exercise is finished.

--- part2/2b/src/App.jsx
+++ part2/2b/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PhoneBook from "./components/PhoneBook";
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
-  return (
-    <PhoneBook persons={persons} setPersons = {setPersons} />
-  );
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((res) => setPersons(res.data));
+  }, []);
+
+  return <PhoneBook persons={persons} setPersons={setPersons} />;
 };
 
 export default App;
